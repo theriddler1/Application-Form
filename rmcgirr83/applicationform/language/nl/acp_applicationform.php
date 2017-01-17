a423089e5da9e7a0d253a335e5701553642c3c6f
Andy Dm <demarieandy@outlook.com>
2017-01-17 18:12:19 +0100

    Add files via upload

---
+++ rmcgirr83/applicationform/language/nl/acp_applicationform.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+*
+*
+* @package - Application Form language
+* @copyright 2016 Rich McGirr (RMcGirr83)
+* Nederlandse vertaling @ Solidjeuh <http://www.froddelpower.be>
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+
+$lang = array_merge($lang, array(
+	'ACP_APPLICATIONFORM_SETTINGS'		=> 'Sollicitatie formulier instellingen',
+	'APPLICATIONFORM_FORUM'				=> 'Forum',
+	'APPLICATIONFORM_FORUM_EXPLAIN'		=> 'Selecteer een forum naar waar de sollicitaties moeten verzonden worden.',
+	'APPLICATIONFORM_POSITIONS'			=> 'Sollicitatie posities',
+	'APPLICATIONFORM_POSITIONS_EXPLAIN'	=> 'Voeg de posities waarvoor gebruikers kunnen solliciteren toe. Elk op een nieuwe lijn.',
+	'APPFORM_SETTINGS_SUCCESS'			=> 'Sollicitatie formulier instellingen werden opgeslagen.',
+	'APPFORM_MUST_HAVE_POSITIONS'		=> 'Je moet op zijn minst 1 positie hebben.',
+	'APPLICATIONFORM_NRU'				=> 'Nieuwe gebruikersgroep toestaan',
+	'APPLICATIONFORM_NRU_EXPLAIN'		=> 'Indien ja zal de nieuw geregistreerde gebruikers groep toegang hebben tot het sollicitatie formulier.',
+	'APPLICATIONFORM_ALLOW_ATTACHMENT'	=> 'Bijlagen toestaan',
+	'APPLICATIONFORM_ALLOW_ATTACHMENT_EXPLAIN' => 'Indien ja zullen bestanden van deze formaten toegestaan worden: doc, pdf en tekst.',
+	'APPLICATIONFORM_ATTACHMENT_REQ'	=> 'Bijlage is verplicht',
+	'APPLICATIONFORM_ATTACHMENT_REQ_EXPLAIN' => 'Indien ja zal het formulier een bijlage nodig hebben voor de sollicitatie waarvoor aanvraag wordt gedaan.',
+));
---
+++ rmcgirr83/applicationform/language/nl/application.php
@@ -0,0 +1,53 @@
+<?php
+/**
+*
+* application [English]
+*
+* @package language
+* @copyright (c) 2016 Rich McGirr (RMcGirr83)
+* Nederlandse vertaling @ Solidjeuh <http://www.froddelpower.be>
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
+$lang = array_merge($lang, array(
+	'APPLICATION_SUBJECT'			=> 'Sollicitatie formulier %s',
+	'APPLICATION_MESSAGE'			=> 'Gebruiker [b]%1$s[/b], heeft een sollicitatie gedaan via het sollicitatie formulier met de volgende informatie.<br>' . "\n" . '[b]Echte naam[/b]: %2$s' . "\n" . '[b]Sollicitatie voor[/b]: %3$s' . "\n" . '[b]Reden voor de sollicitatie:[/b] %4$s',
+	'APPLICATION_SEND'				=> 'Je sollicitatie is succesvol verzonden naar de beheerder. Je mag een antwoord verwachten binnen de 7 dagen.',
+	'APPLICATION_PAGETITLE'			=> 'Sollicitatie formulier',
+
+	'APPLICATION_WELCOME_MESSAGE'	=> 'Welkom op het sollicitatie formulier. We hebben posities openstaan waarvoor jij misschien geschikt bent. Gelieve het onderstaande formulier in te vullen voor de door jou gekozen positie. We wensen je veel geluk!<br><br><strong><em>Items gemarkeerd met een sterretje moeten ingevuld worden!</em></strong>',
+	'APPLICATION_REALNAME'			=> 'Echte naam',
+	'APPLICATION_EMAIL'				=> 'E-mail adres',
+	'APPLICATION_POSITION'			=> 'Welke positie zou je graag willen',
+	'APPLICATION_WHY'				=> 'Waarom zouden we jou kiezen voor deze positie?',
+	'APP_NOT_COMPLETELY_FILLED'		=> 'Je moet alle velden invullen voor deze sollicitatie.',
+	'ADD_ATTACHMENT_EXPLAIN'		=> 'Voeg een bijlage toe om onze beslissing mogelijk te vergemakkelijken voor jou sollicitatie.',
+	'ADD_ATTACHMENT_REQ'			=> '<strong>Je moet een bijlage toevoegen om in aanmerking te komen voor deze sollicitatie</strong>.',
+	'APPLICATION_REQUIRES_ATTACHMENT'	=> 'Gelieve een bijlage toe toe voegen bij de positie waarvoor je solliciteert.',
+));
